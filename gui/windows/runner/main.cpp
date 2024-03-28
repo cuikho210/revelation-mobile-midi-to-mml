1aa2c016a3257f98bdb5a113861d85520f5e0ef9
cuikho210 <tonykms210@gmail.com>
2024-03-29 02:24:51 +0700

    Update window size and window title for linux and windows

--- gui/windows/runner/main.cpp
+++ gui/windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"midi_to_mml", origin, size)) {
+  Win32Window::Size size(480, 720);
+  if (!window.Create(L"Revelation Mobile MIDI to MML", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
