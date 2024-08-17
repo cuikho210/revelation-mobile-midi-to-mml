521c8629df6194a3d4ba8ebd321eb72b64557209
cuikho210 <tonykms210@gmail.com>
2024-08-17 15:42:15 +0700

    match instrument image

--- gui/lib/components/track.dart
+++ gui/lib/components/track.dart
@@ -267,12 +267,38 @@ class TrackTabButton extends GetView<AppController> {
 		required this.track,
 	});
 
+	String getInstrumentImage(SignalInstrument instrumentData) {
+		Map<String, String> instruments = {
+			'0-7': 'piano',
+			'24-25': 'archtop_guitar',
+			'26-31': 'electric_guitar',
+			'32-39': 'bass_guitar',
+			'40-47': 'violin',
+			'75-75': 'nose_flute',
+		};
+
+		var instrumentName = 'piano';
+
+		if (instrumentData.midiChannel == 9) {
+			instrumentName = 'bass_drum';
+		} else {
+			for (var key in instruments.keys) {
+				var bounds = key.split('-').map(int.parse).toList();
+				if (instrumentData.instrumentId >= bounds[0] && instrumentData.instrumentId <= bounds[1]) {
+					instrumentName = instruments[key]!;
+				}
+			}
+		}
+
+		return "assets/icon-instruments/$instrumentName.png";
+	}
+
 	@override
 	Widget build(context) {
 		return Column(children: [
 			Obx(() => TextButton.icon(
 				onPressed: () => controller.currentTrack(track),
-				icon: const ImageIcon(AssetImage("assets/icon-instruments/piano.png")),
+				icon: ImageIcon(AssetImage(getInstrumentImage(track.instrument))),
 				label: Text("Track ${track.index}"),
 				style: ButtonStyle(
 					shape: const WidgetStatePropertyAll(
